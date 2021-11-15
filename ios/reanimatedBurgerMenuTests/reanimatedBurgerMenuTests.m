f02b556aa20324c88c9eafdc9ce0a21ee1509603
Marcuz Gabriel Larsen <marcuzgabriel@gmail.com>
2021-11-15 18:33:05 +0100

    Adding updated Android and iOS folder

--- ios/reanimatedBurgerMenuTests/reanimatedBurgerMenuTests.m
+++ ios/reanimatedBurgerMenuTests/reanimatedBurgerMenuTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reanimatedBurgerMenuTests : XCTestCase
+@interface ReanimatedBurgerMenuTests : XCTestCase
 
 @end
 
-@implementation reanimatedBurgerMenuTests
+@implementation ReanimatedBurgerMenuTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
